64a9462f6b2118d58c5dcd0109287e92d72f3d0c
ejaramillom <ejaramilo@hotmail.com>
2023-01-25 17:04:48 +0000

    002 basic hooks

--- react-interview.js
+++ react-interview.js
@@ -68,41 +68,382 @@ function App() {
       
 // hooks
 
+a hook is a method used to manipulate react state and lifecycle methods
+
 // jsx
+
+javascript xml is a language created to let javascript include html syntax in its declarations. 
+
+const element = <img src={user.avatarUrl}></img>;
+
+when we  have a json that includes two tags outside a div separation
+
+return (
+  <h2>hi there! </h2>
+  <p>this does not work </p>
+);
+
+this does not work in JSX, you cant return more than one "root" JSX element.
+
+This is also not valid javascript code
+
+return (
+  React.createElement('h2', {}, 'Hi there!')
+  React.createElement('p', {}, 'This does not work ')
+);
+
+because you cannot return two things from a function by separate
+
+a workaround is using a div to wrap inside the things you want to return
+or then you can use an array [] but you have to add "key" attribute to each object
+inependently they are a function or an attribute or a JSX object
+
+it does not have to be a div, but this creates the "div soup"
 
 // babel
 
+convert the latest version of JavaScript code into the one that the browser understands. The latest standard version which JavaScript follows is ES2020 which is not fully supported by all the browsers and hence we make use of a tool such as ‘babel’ so that we can convert it into the code that today’s browser understands.
+
 // webpack
+
+javascript library. Webpack is a module bundler that lets you compile JavaScript modules (Files, Images, Fonts, JS, CSS, HTML, etc.). Webpack offers multiple functions, like merging modules, code minimization (or minimizing code by eliminating spaces, remarks, junk code, and code reduction), SASS or TypeScript compiling, integration with npm, and other features.
 
 // redux
 
+a state management system for cross component or app wide state, for example local state state (data) that belongs to single component
+
+eg listening to user input in an input field toggling a show more details field
+
+should be managed component internal with useState/useReducer
+
+cross component state that affects multiple components
+
+eg open closed state in a modal overlay requires 'props chains' or props drilling
+
+app wide state state that affects the entire app
+
+eg user authentication status
+
+requires 'props chains' or 'props drilling'
+
+REDUX SOLVES THE CROSS COMPONENT OR APP WIDE STATE MANAGEMENT
+
+export const BottleDisplay = (props) => {
+  return (
+    <div>
+      <ExportCSV csvData={props.bottlesToDisplay} fileName="bottles" />
+      <br />
+      <br />
+      {props.bottlesToDisplay.map((element) => (
+        <Tile kind="ancestor" className="App">
+          <Tile kind="parent">
+            <Tile renderAs="article" kind="child" notification color="light">
+              <Heading size={7} renderAs="h6">{element.name}
+                <Tag color="dark" className="App"> Drawing </Tag>
+                <Tag color="info"> {element.drawing}</Tag>
+                <Tag color="dark"> Thread </Tag>
+                <Tag color="info"> {element.thread}</Tag>
+                <Tag color="dark"> Depth </Tag>
+                <Tag color="info"> {element.depth}</Tag>
+              <Button
+                type="submit"
+                color="info"
+                onClick={() => {
+                  props.addBottleClick(element);
+                }}
+                size="small"
+              >
+                Add Bottle
+              </Button>
+              </Heading>
+            </Tile>
+          </Tile>
+        </Tile>
+      ))}
+    </div>
+  );
+}
+
 // reducer action and store
+
+reducer function --------------------------------
+
+mutates the store data (these are not reducer hooks)
+
+reducer function => mutation => central data => subscription => components
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+
+actions ---------------------------------
+
+components trigger certain actions
+
+import { useDispatch } from 'react-redux';
+
+import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cart-slice.js'; 
+
+const CartItem = (props) => {
+  const dispatch = useDispatch();
+
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+store -------------------------------------
+
+redux has one central data store for states
+
+one store for an entire app
+
+central data => subscription => components
+
+so the subscription sends the state data to the component
+
+components never manipulate the store data!!!
+
+we use reducer function
+
+components => dispatch => action => forwarded to reducer
+
+import { configureStore } from '@reduxjs/toolkit';
+
+import uiSlice from './ui-slice';
+import cartSlice from './cart-slice';
+
+const store = configureStore({
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
+});
+
+export default store;
 
 // middlewares
 
+The action/reducer pattern is very clean for updating the state within an application. But what if we need to communicate with an external API? Or what if we want to log all of the actions that are dispatched? We need a way to run side effects without disrupting our action/reducer flow.
+
+Middleware allows for side effects to be run without blocking state updates.
+
 // data flow in react
+
+1. An event occurs
+2. An action is dispatched
+3. Middleware receives the action
+4. Reducer creates a new state from the change prescribed by the action
+5. New state is passed into the React app via props
+
+unidirectional data flow
+
+state is passed to the view and to child components
+actions are triggered by the view
+actions can update the state
+the state change is passed to the view and to child components
 
 // redux thunk vs redux saga
 
+middleware handling libraries
+
 // class components vs functional components
+
+functional components were widely adopted with the introduction of hooks in react 16.8
+
+class components before react 16.8 were the only way of instantiating components to handle data flow
+
+Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML via a render() function.
 
 // componentWillUnmount in functional component
 
-// useEffect, useState, useMemo, useCallback, useRef
+so, the class based components cannot use hooks, so we use the lifecycle promises
+
+componentDidMount() => it is called once component is mounted (was evaluated and rendered)
+
+it is equivalent to useEffect(..., []) in functional components with empty dependencies
+
+componentDidUpdate() => it is called once component is updated (was re-evaluated and rendered)
+
+it is equivalent to useEffect(..., [someValue]) in functional components with some dependencies included
+
+componentWillUnmount() => called right before component is unmounted (removed from DOM)
+
+it is equivalent a useEffect function with a cleanUp function useEffect(() => return () => {})
+
+// useEffect, useState, useMemo, useCallback, useRef, useSelector, createSlice
+
+useEffect ---------------------------------------
+
+The useEffect Hook allows you to perform side effects in your components.
+
+useEffect(() => {
+  fetchMoviesHandler();
+}, [fetchMoviesHandler]);
+
+useCallback ------------------------------------
+
+The React useCallback Hook returns a memoized callback function.
+
+Think of memoization as caching a value so that it does not need to be recalculated. This allows us to isolate resource intensive functions so that they will not automatically run on every render. The useCallback Hook only runs when one of its dependencies update.
+
+The useCallback and useMemo Hooks are similar. The main difference is that useMemo returns a memoized value and useCallback returns a memoized function. You can learn more about useMemo in the useMemo chapter.
+
+const fetchMoviesHandler = useCallback(async () => {
+  setIsLoading(true);
+  setError(null);
+  try {
+    const response = await fetch(
+      "https://react-http-59b0a-default-rtdb.firebaseio.com/movies.json"
+    );
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const data = await response.json();
+    const loadedMovies = [];
+
+    for (const key in data) {
+      loadedMovies.push({
+        id: key,
+        title: data[key].title,
+        openingText: data[key].openingText,
+        releaseDate: data[key].releaseDate,
+      });
+    }
+    
+    setMovies(loadedMovies);
+  } catch (error) {
+    setError(error.message);    
+  }
+  setIsLoading(false);
+}, []);    
+
+useRef -------------------------------------
+
+The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated.
+
+It can be used to access a DOM element directly.
+
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+<Card className={classes.input}>
+  <form onSubmit={addUserHandler}>
+  <label htmlFor="username">Username</label>
+  <input
+    id="username"
+    type="text"
+    ref={nameInputRef}
 
 // lifecycle method
 
+
+
 // export default vs export
-
-// portals
-
-// reconciliation
 
 // SSR
 
 // useStrict
 
+// portals
+
+Another issue in react isa semantic problem of making modals thar overlay in an application, for example
+
+return (
+<>
+  <MyModal />
+  <MyInputForm />
+</>
+);
+
+this will render
+
+<section>
+ <h2> Content from another component</h2>
+ <div class="my-modal">
+   <h2> Modal title</h2>
+ </div>
+ <Form>
+   <> more content
+ </Form>
+</section>
+
+so, the modal is a SEMANTIC ERROR because it overlays the application, is like trying to create a button starting from a div, like this
+
+<div onClick={clickHandler}>Click me, i am a bad button </div>
+
+does not mean that because this works, it is a good practice
+
+# PORTALS CHANGE THE LOCATION OF THE MODAL LOCATION IN THE DOM
+
+<div class="my-modal">
+  <h2> Modal title</h2>
+</div>
+
+<section>
+ <h2> Content from another component</h2>
+ <Form>
+   <> more content
+ </Form>
+</section>
+
+Something like this
+
 // fragments
+
+# new limitation: the <div> soup
+
+many div are created so many unnecessary divs add no semantic meaning or structure
+
+<div>
+  <div>
+    <div>
+      <div>
+
+many elements and its a bad idea if you have a 1000 elements 
+
+# React fragments
+
+this problem is solved using fragments: We wrap the elements we created on the component with a built in class named React.fragment
+
+return (
+  <React.Fragment>
+    <h2>Hi there! </h2>
+    <h2>Now this works! </h2>
+  </React.Fragment>
+);
 
 // react router
 
